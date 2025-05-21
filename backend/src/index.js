2f479dcea1637f1cc1b74cdde5210e95fdee3bfb
Dinesh Bahadur Saud <dineshsaud008@gmail.com>
2025-05-22 03:04:29 +0545

    Login & Logout & Update Profile

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 
 import { connectDB } from "./lib/db.js";
 
@@ -7,10 +8,12 @@ import authRoutes from "./routes/auth.route.js";
 
 dotenv.config();
 const app = express();
-// middleware used to extract the json data out of body
-app.use(express.json());
 
 const PORT = process.env.PORT; //reading .env file
+
+// middleware used to extract the json data out of body
+app.use(express.json());
+app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 
