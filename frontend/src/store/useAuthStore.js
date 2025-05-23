dcaac44bd12800ffe6b567a786f11a9c373f2059
Dinesh Bahadur Saud <dineshsaud008@gmail.com>
2025-05-23 21:01:57 +0545

    toast success issue

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -1,5 +1,6 @@
 import { create } from "zustand";
 import { axiosInstance } from "../lib/axios.js";
+import toast from "react-hot-toast";
 
 export const useAuthStore = create((set) => ({
   authUser: null,
@@ -28,7 +29,6 @@ export const useAuthStore = create((set) => ({
       const res = await axiosInstance.post("/auth/signup", data);
       set({ authUser: res.data });
       toast.success("Account created successfully");
-      get().connectSocket();
     } catch (error) {
       toast.error(error.response.data.message);
     } finally {
